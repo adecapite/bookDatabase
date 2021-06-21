2ea323a6be3049a86a9e722af3888ba8409dee63
Anthony DeCapite <anthony.decapite@icloud.com>
2021-06-21 15:39:00 -0400

    working on API commands for get delete.

--- client/src/utilis/API.js
+++ client/src/utilis/API.js
@@ -3,6 +3,11 @@ const API = ("dotenv")
 
 export default{
     getBook: function (query){
-        return axios.get('https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=yourAPIKey')
-    }
+        return axios.get('https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=yourAPIKey');
+    },
+
+    deleteBook: function (id){
+        return axios.delete("/api/books/" + id).then(result => result.data);
+    
+    },
 }
