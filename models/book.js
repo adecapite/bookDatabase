a092726965ce4738ebdf025f173a87303c83443e
Anthony DeCapite <anthony.decapite@icloud.com>
2021-06-24 14:03:08 -0400

    saved code from fatal error with TA.

--- models/book.js
+++ models/book.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
+
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    title: String,
-    author: [{type: String}],
-    description: String,
-    img: String,
-    link: String
+  title: { type: String, required: true },
+  authors: { type: [String], required: true },
+  link: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
 });
 
-const Book = mongoose.model("book", bookSchema)
+const Book = mongoose.model("Book", bookSchema);
 
 module.exports = Book;
